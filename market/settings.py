f76b51e04422f2817fa1455fcf1605e8497e218e
Ian McGregor Aksum <ian.aksum@gmail.com>
2026-03-22 13:38:10 +0100

    swapped back to remove database

--- market/settings.py
+++ market/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = "market.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": dj_database_url.config(
-    #     # Replace this value with your local database's connection string.
-    #     default=os.environ.get("DATABASE_URL"),
-    #     conn_max_age=600,
-    # )
-    "default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": "mydatabase",
+    "default": dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+    # "default": {
+    # "ENGINE": "django.db.backends.sqlite3",
+    # "NAME": "mydatabase",
     }
 }
 
